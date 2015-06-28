18555171b504a6d2c8f6a80f5a77cee5a3b3e574
tienwei <g943942@gmail.com>
2015-06-29 00:31:07 +1000

    First commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name hCardAppApp
+ * @description
+ * # hCardAppApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('hCardAppApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,18 @@
+(function(){
+	'use strict';
+
+	/**
+	 * @ngdoc function
+	 * @name hCardAppApp.controller:MainCtrl
+	 * @description
+	 * # MainCtrl
+	 * Controller of the hCardAppApp
+	 */
+	angular.module('hCardAppApp')
+	  .controller('MainCtrl', MainCtrl);
+
+	function MainCtrl () {
+		var main = this;
+	}	  
+})();
+
